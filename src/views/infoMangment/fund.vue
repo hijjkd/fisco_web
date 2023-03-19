4d079056533a579537c6ef37f3d408b095e0f04b
liunan <737720322@qq.com>
2023-03-19 16:03:35 +0800

    新增发票、融资意向、回款vue组件

---
+++ src/views/infoMangment/fund.vue
@@ -0,0 +1,59 @@
+<template>
+    <el-table :data="InvoiceInfo" style="width: 100%">
+        <el-table-column prop="CustcdLinkPosition" label="联系人职位" width="180">
+        </el-table-column>
+        <el-table-column prop="CustcdLinkName" label="联系人" width="180">
+        </el-table-column>
+        <el-table-column prop="CertificateId" label="供应商证件号码">
+        </el-table-column>
+        <el-table-column prop="CorpName" label="供应商名称">
+        </el-table-column>
+        <el-table-column prop="Remark" label="备注">
+        </el-table-column>
+        <el-table-column prop="BankContact" label="我行联系方式">
+        </el-table-column>
+        <el-table-column prop="BankLinkName" label="我行联系人">
+        </el-table-column>
+        <el-table-column prop="CustcdContact" label="联系方式">
+        </el-table-column>
+
+        <el-table-column prop="CustomerId" label="供应商编号">
+        </el-table-column>
+        <el-table-column prop="FinanceId" label="融资意向申请编号">
+        </el-table-column>
+        <el-table-column prop="CooperationYears" label="合作年限">
+        </el-table-column>
+        <el-table-column prop="CertificateType" label="供应商证件类型">
+        </el-table-column>
+        <el-table-column prop="InterCustomerId" label="核心企业证件号">
+        </el-table-column>
+
+    </el-table>
+</template>
+  
+<script>
+import axios from 'axios'
+export default {
+    name: "fund",
+    components: {
+        "v-content-head": contentHead,
+    },
+    data() {
+        return {
+            InvoiceInfo: []
+        }
+    }, methods: {
+        getData() {
+            axios.get('/api/asl/universal/decryptCollectionAccount').then(response => {
+                console.log("liunan" + response.data);
+                this.InvoiceInfo = response.data
+            },);
+        }
+    }, mounted() {
+        this.getData();
+        // window.addEventListener('click',function(e){
+        //     console.log(e)
+        // })
+    },
+}
+</script>
---
+++ src/views/infoMangment/invoice.vue
@@ -0,0 +1,137 @@
+<template>
+    <el-table
+      :data="InvoiceInfo"
+      style="width: 100%"
+      max-height="950">
+      <el-table-column
+      fixed
+        prop="certificateId"
+        label="供应商证件号码"
+        width="150">
+      </el-table-column>
+      <el-table-column
+      fixed
+        prop="customerId"
+        label="供应商编号"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="corpName"
+        label="供应商名称"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="certificateType"
+        label="供应商证件类型"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="interCustomerId"
+        label="核心企业的证件号"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="InvoiceNotaxAmt"
+        label="发票不含税金额（专票）"
+        width="170">
+      </el-table-column>
+      <el-table-column
+        prop="InvoiceCcy"
+        label="发票币种"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="SellerName"
+        label="销方名称"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="InvoiceType"
+        label="发票类型"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="BuyerName"
+        label="购方名称"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="BuyerUsccode"
+        label="购方税号"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="InvoiceDate"
+        label="开票日期"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="SellerUsccode"
+        label="销方税号"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="InvoiceCode"
+        label="发票代码"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="InvoiceNum"
+        label="发票号码"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="CheckCode"
+        label="校验码后六位"
+        width="120">
+      </el-table-column>
+      <el-table-column
+        prop="InvoiceAmt"
+        label="发票金额"
+        width="120">
+      </el-table-column>
+  
+  
+    </el-table>
+  </template>
+    
+  <style>
+  .demo-table-expand {
+    font-size: 0;
+  }
+  
+  .demo-table-expand label {
+    width: 90px;
+    color: #99a9bf;
+  }
+  
+  .demo-table-expand .el-form-item {
+    margin-right: 0;
+    margin-bottom: 0;
+    width: 50%;
+  }
+  </style>
+  
+  <script>
+  
+  //import axios from 'axios'
+   export default {
+    data() {
+      return {
+        InvoiceInfo: []
+      }
+    }, methods: {
+      //getData() {
+       // axios.get('/api/asl/universal/decryptInvoiceInformation').then(response => {
+        //  console.log("liunan" + response.data);
+  //this.InvoiceInfo = response.data
+        //},);
+      //}
+    },    mounted() {
+          this.getData();
+          // window.addEventListener('click',function(e){
+          //     console.log(e)
+          // })
+      },
+  }
+  </script>
